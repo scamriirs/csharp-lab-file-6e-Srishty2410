e497caafd4b4218e61a83cb96dfefc999984706d
Srishty2410 <srishtysharma2410@gmail.com>
2025-03-10 10:27:26 +0530

    Create program14.cs

---
+++ program14.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Program
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+
+        Console.WriteLine("Sum of 2 and 3: " + program.Add(2, 3));
+
+        Console.WriteLine("Sum of 1, 4, and 5: " + program.Add(1, 4, 5));
+
+        Console.WriteLine("Sum of 2.5 and 3.5: " + program.Add(2.5, 3.5));
+    }
+}
