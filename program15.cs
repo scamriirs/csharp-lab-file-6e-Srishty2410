61fb7cc7574df61900b5dade675d668d34846d60
Srishty2410 <srishtysharma2410@gmail.com>
2025-03-10 10:31:57 +0530

    Create program15.cs

---
+++ program15.cs
@@ -0,0 +1,41 @@
+//Write a program to show the concept of overriding
+
+using System;
+
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("The animal makes a sound.");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("The dog barks.");
+    }
+}
+
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("The cat meows.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.Speak();
+        myDog.Speak();
+        myCat.Speak();
+    }
+}
